ec3e3ce7acb0db5597e61eb530a8b62ac386bb18
Lucas Merckelbach <lucas.merckelbach@hzg.de>
2018-12-12 14:48:22 +0100

    updated version to 1.0.1

--- gliderflight/__init__.py
+++ gliderflight/__init__.py
@@ -1,5 +1,5 @@
 #Copyright (c) 2018 Helmholtz Zentrum Geesthacht, Germany
 #                   Lucas Merckelbach, lucas.merckelbach@hzg.de
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 from .gliderflight import *
