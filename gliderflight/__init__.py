02198c0040d950683caaf2c33ed1e10e080a7455
Lucas Merckelbach <lucas.merckelbach@hzg.de>
2023-05-09 11:24:26 +0200

    updates version to 1.2.0

--- gliderflight/__init__.py
+++ gliderflight/__init__.py
@@ -1,7 +1,7 @@
-__version__ = '1.1.0'
+__version__ = '1.2.0'
 
 #
-#Copyright (c) 2018 Helmholtz Zentrum Geesthacht, Germany
-#                   Lucas Merckelbach, lucas.merckelbach@hzg.de
+#Copyright (c) 2018-2023 Helmholtz Zentrum hereon, Geesthacht, Germany
+#                   Lucas Merckelbach, lucas.merckelbach@hereon.de
 
 from .gliderflight import *
